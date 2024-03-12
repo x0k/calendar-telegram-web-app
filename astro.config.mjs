d2997b74e4f309d05e8ed88f555b2379bdc9ebc5
Krasilnikov Roman <x0kill@ya.ru>
2024-03-12 12:26:09 +0300

    Fix calendar position

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,22 @@ export default defineConfig({
   integrations: [tailwind()],
   site: "https://x0k.github.io",
   base: "/calendar-telegram-web-app",
+  vite: {
+    server: {
+      port: 443,
+      host: "0.0.0.0",
+      hmr: {
+        host: "calendar-telegram-web-app.local",
+        port: 443,
+      },
+      https: {
+        key: fs.readFileSync("./.cert/localhost-key.pem"),
+        cert: fs.readFileSync("./.cert/localhost.pem"),
+      },
+    },
+  },
+  server: {
+    port: 443,
+    host: "calendar-telegram-web-app.local",
+  },
 });
