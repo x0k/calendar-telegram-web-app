b0b4d6a1f2c8844402c9ab35b907a99d938fa523
Krasilnikov Roman <x0kill@ya.ru>
2024-03-13 21:41:25 +0300

    WIP: configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,22 @@ export default defineConfig({
   integrations: [tailwind()],
   site: "https://x0k.github.io",
   base: "/telegram-web-inputs",
+  vite: {
+    server: {
+      port: 443,
+      host: "0.0.0.0",
+      hmr: {
+        host: "telegram-web-inputs.local",
+        port: 443,
+      },
+      https: {
+        key: fs.readFileSync("./.certs/telegram-web-inputs.local.key"),
+        cert: fs.readFileSync("./.certs/telegram-web-inputs.local.crt"),
+      },
+    },
+  },
+  server: {
+    port: 443,
+    host: "telegram-web-inputs.local",
+  }
 });
