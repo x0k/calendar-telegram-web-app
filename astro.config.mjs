351d64b3abf96e92223bc3d8584696a077fb6f81
Krasilnikov Roman <x0kill@ya.ru>
2024-09-11 21:36:16 +0300

    Fix diagram rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,16 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import remarkMermaid from 'astro-diagram/remark-mermaid';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://x0k.github.io",
   base: "/telegram-web-inputs",
+  markdown: {
+    remarkPlugins: [
+      remarkMermaid
+    ]
+  }
 });
